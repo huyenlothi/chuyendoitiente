eaef5c5d1d778a1a33fffe5731bdd72681ac5872
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-08-11 15:48:55 +0700

    chuyendoitiente

--- chuyendoitiente.js
+++ chuyendoitiente.js
@@ -1,10 +1,8 @@
 function convert() {
-    let currency = {"VND":{"USD":0.000043,"YEN":0.00457},"USD":{"VND":23.17,"YEN":105.99},
-    "YEN":{"VND":219.06, "USD":0.0094}};
     let amount = parseInt(document.getElementById("input").value);
-    let from = document.getElementById("from").value;
-    let to = document.getElementById("to").value;
-    let result = amount * currency[from][to];
+    let from = parseInt(document.getElementById("from").value);
+    let to = parseInt(document.getElementById("to").value);
+    let result = amount * (from / to );
     document.getElementById("result1").value = result;
  }
 
